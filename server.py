4d8f4d9f8af4800a2c0e32b587e3e89cad4e9390
Yuriy Zubarev <yuriyzubarev@Yuriys-Mac-mini.local>
2014-11-24 21:07:43 -0800

    executing Bash from Python HTTP server

--- server.py
+++ server.py
@@ -1,34 +1,22 @@
 from BaseHTTPServer import BaseHTTPRequestHandler
 import urlparse
+import subprocess
 
 class GetHandler(BaseHTTPRequestHandler):
     
     def do_GET(self):
         parsed_path = urlparse.urlparse(self.path)
-        message_parts = [
-                'CLIENT VALUES:',
-                'client_address=%s (%s)' % (self.client_address,
-                                            self.address_string()),
-                'command=%s' % self.command,
-                'path=%s' % self.path,
-                'real path=%s' % parsed_path.path,
-                'query=%s' % parsed_path.query,
-                'request_version=%s' % self.request_version,
-                '',
-                'SERVER VALUES:',
-                'server_version=%s' % self.server_version,
-                'sys_version=%s' % self.sys_version,
-                'protocol_version=%s' % self.protocol_version,
-                '',
-                'HEADERS RECEIVED:',
-                ]
-        for name, value in sorted(self.headers.items()):
-            message_parts.append('%s=%s' % (name, value.rstrip()))
-        message_parts.append('')
-        message = '\r\n'.join(message_parts)
+        query = parsed_path.query
+        params = dict(urlparse.parse_qsl(query))
+
+        bashCommand = "./statz.sh " + params["name"] + " " + params["value"] + " " + params["datetime"] + " d-7_5m"
+        process = subprocess.Popen(bashCommand.split(), stdout = subprocess.PIPE)
+
+        output = process.communicate()[0]
+
         self.send_response(200)
         self.end_headers()
-        self.wfile.write(message)
+        self.wfile.write(output)
         return
 
 if __name__ == '__main__':
