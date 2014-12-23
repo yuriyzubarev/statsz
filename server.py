66e8b6d60785ea91e433e303da81e98e06729370
Yuriy Zubarev <yuriyzubarev@Yuriys-Mac-mini.local>
2014-12-22 23:17:48 -0800

    Passing command line parameters to start http(s) server

--- server.py
+++ server.py
@@ -1,13 +1,21 @@
 from BaseHTTPServer import BaseHTTPRequestHandler
+import sys, getopt
 import urlparse
 import subprocess
-import ssl
 
 class GetHandler(BaseHTTPRequestHandler):
     
+    def do_400(self):
+        self.send_response(400)
+        self.end_headers()
+        self.wfile.write("400")
+
     def do_GET(self):
         parsed_path = urlparse.urlparse(self.path)
         query = parsed_path.query
+        if not query:
+            self.do_400()
+            return
         params = dict(urlparse.parse_qsl(query))
 
         bashCommand = "./statsz.sh " + params["name"] + " " + params["value"] + " " + params["datetime"] + " d-7_5m"
@@ -20,9 +28,43 @@ class GetHandler(BaseHTTPRequestHandler):
         self.wfile.write(output)
         return
 
-if __name__ == '__main__':
+def usage():
+    print 'Usage: python ' + sys.argv[0] + ' --host str --port num --ssl bool'
+
+def main(argv):
+    hostname = ''
+    port = 0
+    use_ssl = False
+
+    try:
+        opts, args = getopt.getopt(argv, "h", ["host=", "port=", "ssl="])
+        if not opts:
+            print 'No options supplied'
+            usage()
+            sys.exit(1)
+    except getopt.GetoptError:
+        usage()
+        sys.exit(2)
+    
+    for opt, arg in opts:
+        if opt == '-h':
+            usage()
+            sys.exit()
+        elif opt in ("--host"):
+            hostname = arg
+        elif opt in ("--port"):
+            port = int(arg)
+        elif opt in ("--ssl"):
+            use_ssl = arg in ['True', 'true', 'yes', 'y', '1']
+
     from BaseHTTPServer import HTTPServer
-    server = HTTPServer(('localhost', 8083), GetHandler)
-    server.socket = ssl.wrap_socket (server.socket, certfile='zserver.pem', server_side=True)
-    print 'Starting server, use <Ctrl-C> to stop'
+    server = HTTPServer((hostname, port), GetHandler)
+    if use_ssl:
+        import ssl
+        server.socket = ssl.wrap_socket(server.socket, certfile='zserver.pem', server_side=True)
+    print 'Starting server use <Ctrl-C> to stop'
+    print 'Hostname: %s; port: %s; use ssl: %s' % (hostname, port, use_ssl)
     server.serve_forever()
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
